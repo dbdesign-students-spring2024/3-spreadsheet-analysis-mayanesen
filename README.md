21b5d6bcd94e77563f9e6b3afcc3ca7ce4d19eda
Maya Nesen <mayanesen@10-17-123-120.dynapool.wireless.nyu.edu>
2024-02-19 19:56:59 -0500

    update for data

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # Spreadsheet Analysis
+
+## By Maya Nesen
+
 A little assignment to practice finding data, munging it, and analyzing it in a spreadsheet program.
 
-Replace the contents of this file with a report, as described in the [instructions](./instructions.md).
+### Dataset details
+
+This dataset comes from the [New York City Open Data site](https://data.cityofnewyork.us/Public-Safety/NYPD-Arrest-Data-Year-to-Date-/uip8-fykc/about_data) and is composed of data regarding arrests in New York City, across its various boroughs. Columns and data include descriptions of the crime committed, the borrough where the arrest occurred, age, race, gender, etc. The original format of the dataset was a CSV / Excel file.
+
+Data set details:
+Display some of the raw data from the original data file (the first 20 rows is enough). Use Markdown's ability to display tables - see the examples in the Markdown guide linked above.
+
+The dataset that I chose was surprisingly pretty clean already. The column that caused the most trouble was of the police descriptions of the crimes because there were commas in them which made splitting up the data in the CSV by commas using the `.split()` function into a list challenging. I had to replace any commas in that column with dashes so that I could proceed with the scrubbing.
+There were also a few missing values, usually noted as "null" or with a 0 in my dataset. I replaced them all with the string "NaN" for consistency. Even then, there were not many missing values present in my data.
+To further clean and organize my data, I deleted and added several columns. I deleted columns such as "PD_CD", "LAW CODE", etc which contained codes that are most likely useful to police departments but not so much to the general public. I also deleted a column called "PD_DESC" with the police's descriptions of the arrests since there was also a "OFSC_DESC" column with an additional categorization of the crime type, likely done by the police department. But even then, there were 63 different categories of crimes. While useful surely to the police, I decided to re-organize these crime types into 8 broader categories instead: violent, property, sexual, financial, weapon-related, drug-related, vehicle-related, and miscellaneous crimes. I also added a column with the season in which the arrest occurred to categorize the column with the exact dates of arrest in each row.
+Below are the links to my data files:
+[Original Raw Data]()
+[Munged Data]()
+[Spreadsheet File]()
+Links to your data files (the original raw data, the munged data, and the spreadsheet file including the formulas and charts). These can be links to files in your own repository or links to the files stored in a cloud storage service if your files are too large to be included in your own repository.
+Analysis:
+
+Describe each of the aggregate statistic you have calculated - include a description of each and describe any insights the statistic shows that may not be obvious to someone just viewing the raw data.
+If using a pivot table for analysis, include a Markdown table showing a sample of the results of the pivot table (no more than 20 rows, please), along with a short description of what the results show and any insights they offer.
+If using a chart for visualization, include the chart image in the report, with a short description of what the image shows and any insights it offers. See the Markdown guide linked above for details of showing an image.
--- munge.py
+++ munge.py
@@ -1,1 +1,131 @@
 # place your code to clean up the data file below.
+f = open("data/NYPD_Arrest_Data__Year_to_Date__20240213.csv", "r")
+f_new = open("data/clean_data.csv", "w")
+
+# HEADER LINE
+header = f.readline()
+# split to get rid of spaces between names of columns + replace with commas
+column_names = header.split(",") # split on commas
+# note: ARREST_BORO = Borough of arrest. B(Bronx), S(Staten Island), K(Brooklyn), M(Manhattan), Q(Queens)
+# adding columns
+column_names.append("SEASON")
+column_names.append("CRIME_TYPE")
+# deleting columns
+column_names.pop(2) # delete PD_CD
+column_names.pop(3) # delete KY_CD
+column_names.pop(3) # delete OFSC_DESC
+column_names.pop(3) # delete LAW_CODE
+column_names.pop(5) # delete ARREST_PRECINCT
+column_names.pop(13) # delete New Georeferenced Column
+
+# back to csv format + write to csv
+column_names_str = ','.join(column_names)
+f_new.write(column_names_str + "\n")
+
+#unique_vals = []
+
+all_lines = f.readlines()
+# filter to turn into csv
+for i in range(len(all_lines)):
+    #l_list = all_lines[i].split()
+    l_str = all_lines[i].strip()
+
+    # PD_DESC column has commas IN IT so the .split() function will not work correctly
+    # the ones that DO have commas in it are within " " quotation marks
+    # we clean the lines here to replace commas with dashes "-"
+    quotes = False
+    l_clean = ""
+    for char in l_str:
+        if char == '"':
+            quotes = not quotes
+        elif char == ',' and (quotes == True):
+            char = "-"
+        l_clean += char
+
+    l_list = l_clean.split(",")
+
+    # prints out the dates of arrest for just a particular month (ex: jan "01/")
+    # categorizing winter as Dec, Jan, Feb. spring as Mar, Apr, May, etc etc
+    if l_list[1][:3] in ["12/", "01/", "02/"]:
+        l_list.append("WINTER")
+    elif l_list[1][:3] in ["03/", "04/", "05/"]:
+        l_list.append("SPRING")
+    elif l_list[1][:3] in ["06/", "07/", "08/"]:
+        l_list.append("SUMMER")
+    elif l_list[1][:3] in ["09/", "10/", "11/"]:
+        l_list.append("FALL")
+    else:
+        l_list.append("NaN")
+
+    # treating null values to instead write "NaN" and repeated values
+    if l_list[3] == "(null)":
+        l_list[3] = "NaN"
+    if l_list[5] == "(null)":
+        l_list[5] = "NaN"
+    if l_list[5] == "INTOXICATED & IMPAIRED DRIVING":
+        l_list[5] = 'INTOXICATED/IMPAIRED DRIVING'
+    if (l_list[7] == "") or (l_list[7] == "(null)"):
+        l_list[7] = "NaN"
+    if (l_list[14] == "0"):
+        l_list[14] = "NaN"
+    if (l_list[15] == "0"):
+        l_list[15] = "NaN"
+    if (l_list[16] == "0"):
+        l_list[16] = "NaN"
+    if (l_list[17] == "0"):
+        l_list[17] = "NaN"
+
+    # 0 = ARREST_KEY,1 = ARREST_DATE,3 = PD_DESC,7 =LAW_CAT_CD,8 =ARREST_BORO,
+    # 10 =JURISDICTION_CODE,11 = AGE_GROUP,12 =PERP_SEX, 13 =PERP_RACE
+    # 14 =X_COORD_CD,15 =Y_COORD_CD,16 =Latitude,17 =Longitude
+    # checking columns that I'm keeping for missing values / testing for unique values
+    #if l_list[0] not in unique_vals:
+        #unique_vals.append(l_list[0])
+    
+    # creating categories for type of crime instead of having 63 kinds, narrowing down to 8 types instead
+    # these crimes could of course be organized in a variety of ways, but this is what i did
+    if ("DRUG" in l_list[5]) or ("ALCOHOLIC BEVERAGE" in l_list[5]) or ("CANNABIS" in l_list[5]):
+        l_list.append("DRUG-RELATED")
+        l_list[5] = "DRUG-RELATED"
+    elif ("SEX" in l_list[5]) or (l_list[5] == "RAPE") or ("PROSTITUTION" in l_list[5]):
+        l_list.append("SEXUAL")
+        l_list[5] = "SEXUAL"
+    elif ("WEAPON" in l_list[5]) or ("WEAP" in l_list[5]):
+        l_list.append("WEAPON")
+        l_list[5] = "WEAPON"
+    elif ("HOMICIDE" in l_list[5]) or ("MURDER" in l_list[5]) or ("KIDNAP" in l_list[5]) or ("ASSAULT" in l_list[5]) or ("HARRASS" in l_list[5]) or ("SAFETY" in l_list[5]) or ("THE PERSON" in l_list[5]):
+        l_list.append("VIOLENT")
+        l_list[5] = "VIOLENT"
+    elif ("ROBBERY" in l_list[5]) or ("BURGLAR" in l_list[5]) or ("LARCENY" in l_list[5]) or ("THEF" in l_list[5]) or ("ARSON" in l_list[5]) or ("STOLEN" in l_list[5]) or ("TRESPASS" in l_list[5]):
+        l_list.append("PROPERTY")
+        l_list[5] = "PROPERTY"
+    elif ("FRAUD" in l_list[5]) or ("GAMBLING" in l_list[5]) or ("FORGERY" in l_list[5]) or ("WELFARE" in l_list[5]):
+        l_list.append("FINANCIAL")
+        l_list[5] = "FINANCIAL"
+    elif ("VEHICLE" in l_list[5]) or ("DRIVING" in l_list[5]) or ("TRAFFIC" in l_list[5]) or ("PARKING" in l_list[5]):
+        l_list.append("VEHICLE-RELATED")
+        l_list[5] = "VEHICLE-RELATED"
+    else:
+        l_list.append("MISC")
+        l_list[5] = "MISC"
+    
+    # now deleting columns:
+    l_list.pop(2) # delete PD_CD
+    l_list.pop(3) # delete KY_CD
+    l_list.pop(3) # delete OFSC_DESC
+    l_list.pop(3) # delete LAW_CODE
+    l_list.pop(5) # delete ARREST_PRECINCT
+    l_list.pop(13) # delete New Georeferenced Column
+
+
+    #l_str = ','.join(l_list) # convert back to csv format
+    for j in range(len(l_list)):
+        f_new.write(l_list[j])
+        if j != (len(l_list) - 1):
+            f_new.write(",")
+    f_new.write("\n")
+
+#print(unique_vals)
+
+f_new.close()
+f.close()
